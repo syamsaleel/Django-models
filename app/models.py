ac1b1aa3ab549b708ddd542fff25d8e3475cffea
Syamsaleel <syamsaleel4066@gmail.com>
2023-07-01 21:31:17 +0530

    changes

--- app/models.py
+++ app/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-
-from django.db import models
 from django.contrib.auth.models import User
 
 
@@ -12,18 +10,18 @@ class Profile(models.Model):
     website = models.URLField(max_length=200, null=True, blank=True)
     location = models.CharField(max_length=100, null=True, blank=True)
 
-
-class Post(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    caption = models.TextField(blank=True)
-    content = models.ImageField(upload_to='post_images')
-    publication_date = models.DateTimeField(auto_now_add=True)
-
-
 class Like(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+    
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    caption = models.TextField(blank=True)
+    content = models.ImageField(upload_to='/image')
+    publication_date = models.DateTimeField(auto_now_add=True)
+
+
 
 
 class Comment(models.Model):
